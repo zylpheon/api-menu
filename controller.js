d16ac8ec46e3f1c1b3352012e3c0825ba6191985
zylpheon <lrv94451@gmail.com>
2025-04-02 12:08:35 +0700

    1.09

--- controller.js
+++ controller.js
@@ -6,3 +6,14 @@ var connection = require("./koneksi");
 exports.index = function (req, res) {
   response.ok("API is running", res);
 };
+
+// Menampilkan semua data menu
+exports.tampilSemuaMenu = function (req, res) {
+  connection.query("SELECT * FROM menu", function (error, rows, fields) {
+    if (error) {
+      console.log(error);
+    } else {
+      response.ok(rows, res);
+    }
+  });
+};
--- router.js
+++ router.js
@@ -4,4 +4,7 @@ module.exports = function (app) {
   var jsonku = require("./controller");
 
   app.route("/").get(jsonku.index);
+
+  // Menampilkan semua data menu
+  app.route("/tampil").get(jsonku.tampilSemuaMenu);
 };
