3cef15a97d9e29fc09a71721e19a2a66faa8a760
zylpheon <lrv94451@gmail.com>
2025-04-02 11:47:44 +0700

    1.07

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const app = express();
 // Parse application/json
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+// Import routes
+var routes = require("./router");
+routes(app);
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
