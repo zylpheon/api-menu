558b297c85bb4a964141c0e90000fc44fe781824
zylpheon <lrv94451@gmail.com>
2025-04-03 20:41:05 +0700

    1.16

--- middleware/auth.js
+++ middleware/auth.js
@@ -52,3 +52,66 @@ exports.registrasi = function (req, res) {
     }
   });
 };
+
+// Controller untuk login
+exports.login = function (req, res) {
+  var post = {
+    password: md5(req.body.password),
+    email: req.body.email,
+  };
+
+  var query = "SELECT * FROM ?? WHERE ??=? AND ??=?";
+  var table = ["user", "email", post.email, "password", post.password];
+
+  query = mysql.format(query, table);
+
+  connection.query(query, function (error, rows, fields) {
+    if (error) {
+      console.log(error);
+      return response.ok(
+        { message: "Terjadi kesalahan pada server", error: error.message },
+        res
+      );
+    }
+
+    if (rows.length > 0) {
+      var token = jwt.sign({ rows }, config.secret, {
+        expiresIn: "1h", // expires in 1 hour
+      });
+
+      var id_user = rows[0].id;
+
+      var data = {
+        id_user: id_user,
+        access_token: token,
+        ip: ip.address(),
+      };
+
+      var query = "INSERT INTO ?? SET ?";
+      var table = ["akses_token"];
+
+      query = mysql.format(query, table);
+      connection.query(query, data, function (error, rows, fields) {
+        if (error) {
+          console.log(error);
+          return response.ok(
+            { message: "Terjadi kesalahan pada server", error: error.message },
+            res
+          );
+        } else {
+          // Menggunakan response.ok dari res.js Anda
+          return response.ok(
+            {
+              message: "Berhasil login",
+              token: token,
+              user_id: id_user,
+            },
+            res
+          );
+        }
+      });
+    } else {
+      response.ok({ message: "Email atau password salah" }, res);
+    }
+  });
+};
--- middleware/index.js
+++ middleware/index.js
@@ -5,4 +5,7 @@ var router = express.Router();
 // Daftarkan menu registrasi route
 router.post("/api/v1/register", auth.registrasi);
 
+// Daftarkan menu login route
+router.post("/api/v1/login", auth.login);
+
 module.exports = router;
